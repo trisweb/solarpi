c2c04b729fa4f5d56f7cb947bd75b6672f208d5a
Tristan Harward <trisweb@gmail.com>
2025-03-22 22:55:17 -0400

    Deployment tidying, and style adjustments

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ sudo cp -r ./www-root/* /var/www/
 sudo chown -R $USER:www-data /var/www/
 
 echo "Setting up cron job for refresh-camera..."
-echo "* * * * * root $HOME/bin/refresh-camera" | sudo tee /etc/cron.d/solarpi
+echo "* * * * * root $HOME/bin/refresh-camera > /dev/null 2>&1" | sudo tee /etc/cron.d/solarpi > /dev/null
 
 echo "Done!"
