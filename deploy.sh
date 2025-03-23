5acefee0e869e06b02250b8e3233c5dd06823ac2
Tristan Harward <trisweb@gmail.com>
2025-03-22 22:35:41 -0400

    Deployment and camera settings

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Deploying solarpi!"
+
+echo "Installing refresh-camera script..."
+mkdir -p $HOME/bin
+cp ./bin/* $HOME/bin
+
+echo "Copying web root files..."
+sudo mkdir -p /var/www
+sudo cp -r ./www-root/* /var/www/
+sudo chown -R $USER:www-data /var/www/
+
+echo "Setting up cron job for refresh-camera..."
+echo "* * * * * root $HOME/bin/refresh-camera" | sudo tee /etc/cron.d/solarpi
+
+echo "Done!"
